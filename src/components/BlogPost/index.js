fb62b4ff97955a1a20db0dd6ca6a27dc4065d58b
Anıl Şenay <anilsenay3@gmail.com>
2020-09-25 22:04:26 +0300

    Created blog post component for blog page

---
+++ src/components/BlogPost/index.js
@@ -0,0 +1,43 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import styles from "./blog_post.module.scss"
+import DateIcon from "../../assets/date.svg"
+import TagIcon from "../../assets/tag.svg"
+
+export default function BlogPost({ data }) {
+  console.log(data)
+  return (
+    <Link to={data.frontmatter.slug}>
+      <div className={styles.container}>
+        <div className={styles.articleInfo}>
+          <div className={styles.title}>{data.frontmatter.title}</div>
+          <div className={styles.info}>
+            <DateIcon />
+            <span className={styles.dateText}>
+              {new Date(data.frontmatter.date).toLocaleDateString("en-US", {
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+              })}
+            </span>
+            <span className={styles.dot}>·</span>
+            <span className={styles.readMinute}>
+              ~{parseInt(data.wordCount.words / 200) + 1} min read
+            </span>
+          </div>
+          <p className={styles.shortText}>{data.excerpt}</p>
+
+          <div className={styles.tags}>
+            <TagIcon />
+            {data.frontmatter.keywords?.split(", ").map(keyword => (
+              <span className={styles.tagText} key={keyword}>
+                #{keyword}
+              </span>
+            ))}
+          </div>
+        </div>
+      </div>
+    </Link>
+  )
+}
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,12 +2,44 @@ import React from "react"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
+import Articles from "../components/Articles"
+import SearchBar from "../components/SearchBar"
 
-const BlogPage = () => (
+import styles from "./blog.module.scss"
+import BlogPost from "../components/BlogPost"
+
+const BlogPage = ({ data }) => (
   <Layout>
     <SEO title="Blog" />
-    <div></div>
+    <div className={styles.container}>
+      <SearchBar />
+      <div className={styles.articles}>
+        {data.allMarkdownRemark.nodes.map(post => {
+          return <BlogPost data={post} />
+        })}
+      </div>
+    </div>
   </Layout>
 )
+
+export const pageQuery = graphql`
+  {
+    allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
+      nodes {
+        frontmatter {
+          date
+          slug
+          title
+          keywords
+          icon
+        }
+        excerpt(pruneLength: 300)
+        wordCount {
+          words
+        }
+      }
+    }
+  }
+`
 
 export default BlogPage
