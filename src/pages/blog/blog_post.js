918eff34e1e2cc4c56fc8cb359aaf3137aec35f6
Anıl Şenay <anilsenay3@gmail.com>
2020-09-27 21:20:35 +0300

    Add blog post content

--- src/pages/blog/blog_post.js
+++ src/pages/blog/blog_post.js
@@ -1,8 +1,17 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
 
+import styles from "./post.module.scss"
+
+import Layout from "../../components/Layout"
+import SEO from "../../components/seo"
+
+import DateIcon from "../../assets/date.svg"
+import TagIcon from "../../assets/tag.svg"
+
 export default function Post({ data }) {
   const post = data.markdownRemark
+  console.log(post)
 
   if (data === null) {
     return (
@@ -18,7 +27,41 @@ export default function Post({ data }) {
     )
   }
 
-  return <div>{post.frontmatter.title}</div>
+  return (
+    <Layout>
+      <SEO title={post.frontmatter.title} />
+      <div className={styles.container}>
+        <h1 className={styles.titleText}>{post.frontmatter.title}</h1>
+        <div className={styles.info}>
+          <DateIcon />
+          <span className={styles.dateText}>
+            {new Date(post.frontmatter.date).toLocaleDateString("en-US", {
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+            })}
+          </span>
+          <span className={styles.dot}>·</span>
+          <span className={styles.readMinute}>
+            ~{parseInt(post.wordCount.words / 200) + 1} min read
+          </span>
+          <a className={styles.tags}>
+            <TagIcon />
+            {post.frontmatter.keywords?.split(", ").map(keyword => (
+              <span className={styles.tagText} key={keyword}>
+                #{keyword}
+              </span>
+            ))}
+          </a>
+        </div>
+        <hr />
+        <div
+          className={styles.content}
+          dangerouslySetInnerHTML={{ __html: post.html }}
+        />
+      </div>
+    </Layout>
+  )
 }
 
 export const query = graphql`
