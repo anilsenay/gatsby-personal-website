415d291ec2ec2fa3978bda220d12fbab70ef597c
Anıl Şenay <anilsenay3@gmail.com>
2020-09-28 16:04:19 +0300

    Change home page headers

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import Articles from "../components/Articles"
 import styles from "./index.module.scss"
 import Projects from "../components/Projects"
 import { useGetRepos } from "../hooks/getRepos"
+import { Link } from "gatsby"
 
 const IndexPage = ({ data }) => {
   const repos = useGetRepos(
@@ -16,17 +17,32 @@ const IndexPage = ({ data }) => {
     <Layout showContact currentPage="Home">
       <SEO title="Home" />
       <div className={styles.container}>
-        <h1>Anıl Şenay</h1>
-        <h2>Personal Website</h2>
+        <h1>Hey, I'm Anıl Şenay</h1>
+        <h2>I'm a Front-end developer</h2>
         <p className={styles.whoami}>
-          Hi, I am Anıl. I am studying Computer Engineering at Marmara
-          University. I've been interested in computers and programming since my
-          childhood. <span className={styles.underline}>Coding</span> and{" "}
+          I am studying Computer Engineering at Marmara University. I've been
+          interested in computers and programming since my childhood.{" "}
+          <span className={styles.underline}>Coding</span> and{" "}
           <span className={styles.underline} style={{ borderColor: "#EA4C89" }}>
             design
           </span>
-          , which I started with a curiosity and passion, is now a big part of
-          my life. I am interested in Front-end mostly.
+          , which I started with curiosity and passion, is now a big part of my
+          life. I am interested in Front-end mostly.
+          <p className={styles.learnMoreText}>
+            You can read my{" "}
+            <Link to="/blog" className={styles.underline}>
+              blog
+            </Link>
+            , look at my{" "}
+            <Link to="/projects" className={styles.underline}>
+              projects
+            </Link>
+            , or learn more{" "}
+            <Link to="/about" className={styles.underline}>
+              about
+            </Link>{" "}
+            me.
+          </p>
         </p>
 
         <Articles lastestArticles={data.allMarkdownRemark.nodes} />
