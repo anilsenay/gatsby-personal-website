3ae9df63b85534aea7ad70bdf6845ad30404f915
Anıl Şenay <anilsenay3@gmail.com>
2020-09-28 20:01:52 +0300

    Add header for about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,10 +3,14 @@ import React from "react"
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
 
+import styles from "./about.module.scss"
+
 const AboutPage = () => (
   <Layout currentPage="About">
     <SEO title="About" />
-    <div></div>
+    <div className={styles.container}>
+      <h1 className={styles.title}>About me</h1>
+    </div>
   </Layout>
 )
 
