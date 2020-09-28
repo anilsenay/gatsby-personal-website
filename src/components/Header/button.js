2e010943d4d12947c6fb39020506859c6c8bfc36
Anıl Şenay <anilsenay3@gmail.com>
2020-09-29 02:32:58 +0300

    Set header mobile responsive

---
+++ src/components/Header/button.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+import MenuIcon from "../../assets/menu.svg"
+
+export default function HeaderButton({ ...props }) {
+  return (
+    <div
+      style={{
+        width: 48,
+        height: 36,
+        backgroundColor: "transparent",
+        border: "1px solid #20383d",
+        borderRadius: 6,
+        cursor: "pointer",
+      }}
+      {...props}
+    >
+      <MenuIcon width={24} height={24} fill="white" />
+    </div>
+  )
+}
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,52 +1,78 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import React, { useState } from "react"
 
 import styles from "./header.module.scss"
+import Close from "../../assets/close.svg"
+import HeaderButton from "./button"
 
-const Header = ({ currentPage }) => (
-  <header className={styles.container}>
-    <div className={styles.navigation}>
-      <Link
-        to="/"
-        className={styles.links}
-        style={{ color: currentPage === "Home" ? "#82f9a1" : "" }}
-      >
-        Home
-      </Link>
-      <Link
-        to="/projects"
-        className={styles.links}
-        style={{ color: currentPage === "Projects" ? "#82f9a1" : "" }}
-      >
-        Projects
-      </Link>
-      <Link
-        to="/blog"
-        className={styles.links}
-        style={{ color: currentPage === "Blog" ? "#82f9a1" : "" }}
-      >
-        Blog
-      </Link>
-      <div className={styles.rightLinks}>
+const Header = ({ currentPage }) => {
+  const [showHeader, setShowHeader] = useState({})
+
+  return (
+    <header className={styles.container}>
+      <HeaderButton
+        className={styles.headerButton}
+        onClick={() => setShowHeader({ transform: "translate3d(0vw, 0, 0)" })}
+      />
+
+      <div className={styles.navigation} style={showHeader}>
         <Link
-          to="/about"
-          className={styles.links_small}
-          style={{ color: currentPage === "About" ? "#82f9a1" : "" }}
+          to="/"
+          className={styles.links}
+          style={{ color: currentPage === "Home" ? "#82f9a1" : "" }}
         >
-          About
+          Home
         </Link>
         <Link
-          to="/contact"
-          className={styles.links_small}
-          style={{ color: currentPage === "Contact" ? "#82f9a1" : "" }}
+          to="/projects"
+          className={styles.links}
+          style={{ color: currentPage === "Projects" ? "#82f9a1" : "" }}
         >
-          Contact
+          Projects
         </Link>
+        <Link
+          to="/blog"
+          className={styles.links}
+          style={{ color: currentPage === "Blog" ? "#82f9a1" : "" }}
+        >
+          Blog
+        </Link>
+        <div className={styles.rightLinks}>
+          <Link
+            to="/about"
+            className={styles.links_small}
+            style={{ color: currentPage === "About" ? "#82f9a1" : "" }}
+          >
+            About
+          </Link>
+          <Link
+            to="/contact"
+            className={styles.links_small}
+            style={{ color: currentPage === "Contact" ? "#82f9a1" : "" }}
+          >
+            Contact
+          </Link>
+
+          <div
+            className={styles.closeButton}
+            onClick={() =>
+              setShowHeader({ transform: "translate3d(-100vw, 0, 0)" })
+            }
+          >
+            <Close className={styles.close} fill="white" />
+          </div>
+          <div
+            className={styles.background}
+            onClick={() =>
+              setShowHeader({ transform: "translate3d(-100vw, 0, 0)" })
+            }
+          />
+        </div>
       </div>
-    </div>
-  </header>
-)
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
