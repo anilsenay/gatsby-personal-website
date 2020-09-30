5455d70dc01561b96e72ee6ac49ddd6a0a716eeb
Anıl Şenay <anilsenay3@gmail.com>
2020-09-30 16:15:03 +0300

    Set footer mobile responsive

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -22,16 +22,16 @@ export default function Footer() {
       </div>
       <div className={styles.socialIcons}>
         <a href="https://twitter.com/anilsenay">
-          <Twitter width={20} height={20} fill="#ffffff" />
+          <Twitter fill="#ffffff" className={styles.icon} />
         </a>
         <a href="https://instagram.com/anilsenay">
-          <Instagram width={20} height={20} fill="#ffffff" />
+          <Instagram fill="#ffffff" className={styles.icon} />
         </a>
         <a href="https://github.com/anilsenay">
-          <Github width={20} height={20} fill="#ffffff" />
+          <Github fill="#ffffff" className={styles.icon} />
         </a>
         <a href="https://linkedin.com/in/anılşenay">
-          <Linkedin width={20} height={20} fill="#ffffff" />
+          <Linkedin fill="#ffffff" className={styles.icon} />
         </a>
       </div>
     </div>
