78ef2c25b84faaf0475aaba54547ea2302cd9d5f
Anıl Şenay <anilsenay3@gmail.com>
2020-10-01 02:47:36 +0300

    Set 404 page centered

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,8 +6,18 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        marginTop: 50,
+        minHeight: "60vh",
+      }}
+    >
+      <h1>404: Not Found</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </div>
   </Layout>
 )
 
